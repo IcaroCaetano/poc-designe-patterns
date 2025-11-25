9c90a1b0f2722025c5f79960dad0a7901ed543c8
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-24 22:32:33 -0300

    Update Notifier.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/decorator/Notifier.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/decorator/Notifier.java
@@ -1,1 +1,9 @@
+package com.myprojecticaro.poc_designe_patterns.structural.decorator;
 
+public class EmailNotifier implements Notifier {
+
+    @Override
+    public void send(String message) {
+        System.out.println("📧 Sending EMAIL: " + message);
+    }
+}
