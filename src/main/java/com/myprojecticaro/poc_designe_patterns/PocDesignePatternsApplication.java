6c940461d076aa8345f4d7ecb7198a9e176d1065
IcaroCaetano <icaro.caetano@gmail.com>
2025-11-24 22:28:13 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/PocDesignePatternsApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_designe_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDesignePatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocDesignePatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_designe_patterns/PocDesignePatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_designe_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDesignePatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
