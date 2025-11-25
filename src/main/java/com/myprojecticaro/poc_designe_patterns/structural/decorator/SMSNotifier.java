58d76be131b52e48e950f43a68f45a546b3ad6b5
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-24 22:35:59 -0300

    Create SMSNotifier.java

---
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/decorator/SMSNotifier.java
@@ -0,0 +1,14 @@
+package com.myprojecticaro.poc_designe_patterns.structural.decorator;
+
+public class SMSNotifier extends BaseDecorator {
+
+    public SMSNotifier(Notifier wrapped) {
+        super(wrapped);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        System.out.println("📱 Sending SMS: " + message);
+    }
+}
