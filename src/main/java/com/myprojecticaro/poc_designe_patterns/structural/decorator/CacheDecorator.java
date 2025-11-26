820f7b1097914f2195cf7899b6c33f5d6ec085cc
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-25 22:43:50 -0300

    Create CacheDecorator.java

---
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/decorator/CacheDecorator.java
@@ -0,0 +1,14 @@
+public class CacheDecorator implements ExpensiveService {
+
+    private final ExpensiveService wrapped;
+    private final Map<Integer, Integer> cache = new HashMap<>();
+
+    public CacheDecorator(ExpensiveService wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public int calculate(int input) {
+        return cache.computeIfAbsent(input, wrapped::calculate);
+    }
+}
