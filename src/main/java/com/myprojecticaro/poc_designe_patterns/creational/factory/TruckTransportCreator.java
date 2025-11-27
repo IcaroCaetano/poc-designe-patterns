9c5fa1b7db0f91f4d9d250eba83eca06225dc5f2
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-26 21:59:08 -0300

    Update TruckTransportCreator.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/factory/TruckTransportCreator.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/factory/TruckTransportCreator.java
@@ -1,1 +1,9 @@
+package creational.factorymethod.transport;
+
+public class TruckTransportCreator extends TransportCreator {
+    @Override
+    public Transport createTransport() {
+        return new TruckTransport();
+    }
+}
 
