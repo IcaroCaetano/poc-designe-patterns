c713f5773068c26c91e9a4c9eb9053796333a25a
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-28 20:40:43 -0300

    Update OldPaymentGateway.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/adapter/OldPaymentGateway.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/adapter/OldPaymentGateway.java
@@ -1,1 +1,10 @@
+package com.myprojecticaro.poc_designe_patterns.structural.adapter;
+
+public class OldPaymentGateway {
+
+    public void makePayment(double amount, String currency) {
+        System.out.println("Pagamento processado no sistema ANTIGO:");
+        System.out.println("Valor: " + amount + " | Moeda: " + currency);
+    }
+}
 
