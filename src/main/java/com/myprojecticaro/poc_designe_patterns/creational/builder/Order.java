0361a24aba14c58d84088bcebc920a4cb01c7d4d
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-27 22:26:37 -0300

    Update Order.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/Order.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/Order.java
@@ -1,1 +1,39 @@
+package creational.builder.order;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+
+    private String customer;
+    private List<String> items;
+    private double totalPrice;
+    private String deliveryAddress;
+    private LocalDate deliveryDate;
+    private boolean express;
+
+    public Order(String customer, List<String> items, double totalPrice,
+                 String deliveryAddress, LocalDate deliveryDate, boolean express) {
+        this.customer = customer;
+        this.items = items;
+        this.totalPrice = totalPrice;
+        this.deliveryAddress = deliveryAddress;
+        this.deliveryDate = deliveryDate;
+        this.express = express;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Order{
+                    customer='%s',
+                    items=%s,
+                    totalPrice=%.2f,
+                    deliveryAddress='%s',
+                    deliveryDate=%s,
+                    express=%s
+                }
+                """.formatted(customer, items, totalPrice, deliveryAddress, deliveryDate, express);
+    }
+}
 
