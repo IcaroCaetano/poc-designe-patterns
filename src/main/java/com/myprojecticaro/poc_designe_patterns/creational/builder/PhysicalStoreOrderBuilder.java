6a411ea3c6d8ab0a78c047b027d0a66602098d48
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-27 22:31:12 -0300

    Update PhysicalStoreOrderBuilder.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/PhysicalStoreOrderBuilder.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/PhysicalStoreOrderBuilder.java
@@ -1,1 +1,54 @@
+package creational.builder.order;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class PhysicalStoreOrderBuilder implements OrderBuilder {
+
+    private String customer;
+    private List<String> items;
+    private double totalPrice;
+    private String pickupStore;
+    private LocalDate pickupDate;
+
+    @Override
+    public OrderBuilder setCustomer(String customer) {
+        this.customer = customer;
+        return this;
+    }
+
+    @Override
+    public OrderBuilder setItems(List<String> items) {
+        this.items = items;
+        return this;
+    }
+
+    @Override
+    public OrderBuilder setTotalPrice(double price) {
+        this.totalPrice = price;
+        return this;
+    }
+
+    @Override
+    public OrderBuilder setDeliveryAddress(String address) {
+        this.pickupStore = address;
+        return this;
+    }
+
+    @Override
+    public OrderBuilder setDeliveryDate(LocalDate date) {
+        this.pickupDate = date;
+        return this;
+    }
+
+    @Override
+    public OrderBuilder setExpress(boolean express) {
+        return this;
+    }
+
+    @Override
+    public Order build() {
+        return new Order(customer, items, totalPrice, pickupStore, pickupDate, false);
+    }
+}
 
