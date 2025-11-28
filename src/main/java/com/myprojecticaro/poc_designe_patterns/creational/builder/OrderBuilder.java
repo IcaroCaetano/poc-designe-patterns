0b18f91b45188e350ff18c61058f0167a444808b
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-27 22:28:52 -0300

    Update OrderBuilder.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/OrderBuilder.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/OrderBuilder.java
@@ -1,1 +1,22 @@
+package creational.builder.order;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface OrderBuilder {
+
+    OrderBuilder setCustomer(String customer);
+
+    OrderBuilder setItems(List<String> items);
+
+    OrderBuilder setTotalPrice(double price);
+
+    OrderBuilder setDeliveryAddress(String address);
+
+    OrderBuilder setDeliveryDate(LocalDate date);
+
+    OrderBuilder setExpress(boolean express);
+
+    Order build();
+}
 
