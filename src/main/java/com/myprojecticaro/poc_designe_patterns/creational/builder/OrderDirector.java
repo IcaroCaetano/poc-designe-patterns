8115c8f6aac76ed1ee84e84c54f8ebcda0468834
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-27 22:33:10 -0300

    Update OrderDirector.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/OrderDirector.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/builder/OrderDirector.java
@@ -1,1 +1,30 @@
+package creational.builder.order;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class OrderDirector {
+
+    public Order createBasicOnlineOrder(OrderBuilder builder) {
+        return builder
+                .setCustomer("John Doe")
+                .setItems(Arrays.asList("Mouse", "Keyboard"))
+                .setTotalPrice(450.0)
+                .setDeliveryAddress("Rua das Flores, 123")
+                .setDeliveryDate(LocalDate.now().plusDays(3))
+                .setExpress(false)
+                .build();
+    }
+
+    public Order createExpressOnlineOrder(OrderBuilder builder) {
+        return builder
+                .setCustomer("Ana Silva")
+                .setItems(Arrays.asList("Notebook"))
+                .setTotalPrice(5500.0)
+                .setDeliveryAddress("Av Central, 900")
+                .setDeliveryDate(LocalDate.now().plusDays(1))
+                .setExpress(true)
+                .build();
+    }
+}
 
