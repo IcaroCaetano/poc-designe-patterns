73ce54c8b348bdc06cdab627f3bd252139c67324
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-29 18:05:50 -0300

    Create EmailObserver.java

---
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/observer/EmailObserver.java
@@ -0,0 +1,9 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.observer;
+
+public class EmailObserver implements Observer {
+
+    @Override
+    public void update(String eventMessage) {
+        System.out.println("📧 Sending EMAIL about event: " + eventMessage);
+    }
+}
