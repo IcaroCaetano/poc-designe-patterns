f9a19ec12db006d8c3d89a29b5b5cf8b96725127
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-29 18:06:37 -0300

    Update FraudAnalysisObserver.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/observer/FraudAnalysisObserver.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/observer/FraudAnalysisObserver.java
@@ -1,1 +1,10 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.observer;
+
+public class FraudAnalysisObserver implements Observer {
+
+    @Override
+    public void update(String eventMessage) {
+        System.out.println("🛑 Running FRAUD ANALYSIS for event: " + eventMessage);
+    }
+}
 
