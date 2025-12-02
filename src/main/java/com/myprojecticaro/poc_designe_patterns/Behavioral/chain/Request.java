7bed6ea9dbd0a76d7677e7ff421869a66f9537e9
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-01 23:38:42 -0300

    Update Request.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/Request.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/Request.java
@@ -1,1 +1,32 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.chain;
+
+public class Request {
+    private final String cpf;
+    private boolean documentValid;
+    private int fraudScore;
+
+    public Request(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public boolean isDocumentValid() {
+        return documentValid;
+    }
+
+    public void setDocumentValid(boolean documentValid) {
+        this.documentValid = documentValid;
+    }
+
+    public int getFraudScore() {
+        return fraudScore;
+    }
+
+    public void setFraudScore(int fraudScore) {
+        this.fraudScore = fraudScore;
+    }
+}
 
