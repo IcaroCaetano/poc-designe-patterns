dece4fd0660655fbb19a60c33829fb923d884338
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-02 23:47:23 -0300

    Implement CPF validation in ValidateCpfHandler

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/ValidateCpfHandler.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/ValidateCpfHandler.java
@@ -1,1 +1,15 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.chain;
+
+public class ValidateCpfHandler extends AbstractHandler {
+
+    @Override
+    public void handle(Request request) {
+        if (request.getCpf() == null || request.getCpf().length() != 11) {
+            System.out.println("❌ CPF inválido!");
+            return; // para a cadeia
+        }
+        System.out.println("✔ CPF validado.");
+        next(request);
+    }
+}
 
