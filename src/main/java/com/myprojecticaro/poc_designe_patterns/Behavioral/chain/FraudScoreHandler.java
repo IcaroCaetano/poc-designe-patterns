810bad4f2e80d58fc92ac168293726710ee25abf
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-02 23:49:54 -0300

    Implement fraud score calculation in handler

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/FraudScoreHandler.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/FraudScoreHandler.java
@@ -1,1 +1,15 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.chain;
+
+import java.util.Random;
+
+public class FraudScoreHandler extends AbstractHandler {
+
+    @Override
+    public void handle(Request request) {
+        int score = new Random().nextInt(100);
+        request.setFraudScore(score);
+        System.out.println("Fraud score calculated:" + score);
+        next(request);
+    }
+}
 
