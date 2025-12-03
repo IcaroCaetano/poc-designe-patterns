cbdc1011b0ce5750faf37b71d0787b9e86bb057e
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-02 23:51:02 -0300

    Fix print statement syntax in FinalApprovalHandler

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/FinalApprovalHandler.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/FinalApprovalHandler.java
@@ -5,7 +5,7 @@ public class FinalApprovalHandler extends AbstractHandler {
     @Override
     public void handle(Request request) {
         if (!request.isDocumentValid() || request.getFraudScore() > 70) {
-            System.out.println("Request rejected due to risk."");
+            System.out.println("Request rejected due to risk.");
         } else {
             System.out.println("Order successfully approved!");
         }
