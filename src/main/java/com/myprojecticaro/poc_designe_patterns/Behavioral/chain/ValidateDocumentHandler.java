adf5437b3effa3819475aef1bd244b4f3ab469e2
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-02 23:49:08 -0300

    Implement document validation in handler

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/ValidateDocumentHandler.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/ValidateDocumentHandler.java
@@ -1,1 +1,12 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.chain;
+
+public class ValidateDocumentHandler extends AbstractHandler {
+
+    @Override
+    public void handle(Request request) {
+        request.setDocumentValid(true);
+        System.out.println("Document validated.");
+        next(request);
+    }
+}
 
