45dd9d7b1ec9b36b4f1031a8c57e8ef70e813056
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-02 23:50:50 -0300

    Implement FinalApprovalHandler for request processing

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/FinalApprovalHandler.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/FinalApprovalHandler.java
@@ -1,1 +1,14 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.chain;
+
+public class FinalApprovalHandler extends AbstractHandler {
+
+    @Override
+    public void handle(Request request) {
+        if (!request.isDocumentValid() || request.getFraudScore() > 70) {
+            System.out.println("Request rejected due to risk."");
+        } else {
+            System.out.println("Order successfully approved!");
+        }
+    }
+}
 
