3368a1d3286fec5f5365195f0280d6adb4492ad1
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-02 23:47:44 -0300

    Change error message for invalid CPF validation

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/ValidateCpfHandler.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/ValidateCpfHandler.java
@@ -5,7 +5,7 @@ public class ValidateCpfHandler extends AbstractHandler {
     @Override
     public void handle(Request request) {
         if (request.getCpf() == null || request.getCpf().length() != 11) {
-            System.out.println("❌ CPF inválido!");
+            System.out.println("Invalid CPF!");
             return; // para a cadeia
         }
         System.out.println("✔ CPF validado.");
