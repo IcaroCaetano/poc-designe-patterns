1719727cfcd614dec3d4f9a3536f5cad0a3bc3cd
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-02 23:46:48 -0300

    Update AbstractHandler.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/AbstractHandler.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/chain/AbstractHandler.java
@@ -1,1 +1,19 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.chain;
+
+public abstract class AbstractHandler implements Handler {
+
+    protected Handler next;
+
+    @Override
+    public Handler setNext(Handler next) {
+        this.next = next;
+        return next;
+    }
+
+    protected void next(Request request) {
+        if (next != null) {
+            next.handle(request);
+        }
+    }
+}
 
