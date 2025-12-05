09646fad5f57774d3a59ce96f62b767bfa7ba8d7
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-04 23:45:28 -0300

    Implement BasicFraudCheck class with validations

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/BasicFraudCheck.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/BasicFraudCheck.java
@@ -1,1 +1,16 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.template_method;
+
+public class BasicFraudCheck extends AbstractFraudCheck {
+
+    @Override
+    protected void specificValidation(Request request) {
+        System.out.println("Basic validation: checking CPF format…");
+    }
+
+    @Override
+    protected void calculateRiskScore(Request request) {
+        System.out.println("Calculating basic fraud score…");
+        request.setFraudScore(30); // baixo risco
+    }
+}
 
