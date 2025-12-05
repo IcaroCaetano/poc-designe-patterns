78d3791472439bcf3ce91cbb908f49ac0357ace0
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-04 23:46:03 -0300

    Update AdvancedFraudCheck.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/AdvancedFraudCheck.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/AdvancedFraudCheck.java
@@ -1,1 +1,16 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.template_method;
+
+public class AdvancedFraudCheck extends AbstractFraudCheck {
+
+    @Override
+    protected void specificValidation(Request request) {
+        System.out.println("Advanced validation: running biometrics & device fingerprinting…");
+    }
+
+    @Override
+    protected void calculateRiskScore(Request request) {
+        System.out.println("Calculating advanced risk score using ML…");
+        request.setFraudScore(85); // risco alto
+    }
+}
 
