1f198736b73cb4ad333745c3676f17ae94738111
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-04 23:46:31 -0300

    Add fraudScore property to Request class

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/Request.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/Request.java
@@ -1,1 +1,15 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.template_method;
+
+public class Request {
+
+    private int fraudScore;
+
+    public int getFraudScore() {
+        return fraudScore;
+    }
+
+    public void setFraudScore(int fraudScore) {
+        this.fraudScore = fraudScore;
+    }
+}
 
