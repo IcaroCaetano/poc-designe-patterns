c27949f0768789cd4fcc0fc9871a23c24ea613b2
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-04 23:47:04 -0300

    Implement TemplateMethodDemo for fraud checks

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/TemplateMethodDemo.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/template/TemplateMethodDemo.java
@@ -1,1 +1,18 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.template_method;
+
+public class TemplateMethodDemo {
+
+    public static void main(String[] args) {
+
+        Request request = new Request();
+
+        System.out.println("---- BASIC FRAUD CHECK ----");
+        AbstractFraudCheck basic = new BasicFraudCheck();
+        basic.process(request);
+
+        System.out.println("\n---- ADVANCED FRAUD CHECK ----");
+        AbstractFraudCheck advanced = new AdvancedFraudCheck();
+        advanced.process(request);
+    }
+}
 
