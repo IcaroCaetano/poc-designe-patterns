e7ac295e2f286d4421e219bd788c66b93c60d3e3
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-09 23:28:36 -0300

    Update Command.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/command/Command.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/command/Command.java
@@ -1,1 +1,9 @@
 
+package com.myprojecticaro.poc_designe_patterns.Behavioral.command;
+
+/**
+ * The Command interface declares a single method for executing an action.
+ */
+public interface Command {
+    void execute();
+}
