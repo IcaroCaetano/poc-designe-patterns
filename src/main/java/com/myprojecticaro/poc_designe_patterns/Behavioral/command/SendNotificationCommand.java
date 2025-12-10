d9f02d892df04f45c11bcfa5d2a1171693c98f32
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-09 23:29:13 -0300

    Update SendNotificationCommand.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/command/SendNotificationCommand.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/command/SendNotificationCommand.java
@@ -1,1 +1,23 @@
+package com.myprojecticaro.poc_designe_patterns.Behavioral.command;
+
+import com.myprojecticaro.poc_designe_patterns.structural.decorator.Notifier;
+
+/**
+ * A concrete Command that encapsulates a request to send a notification.
+ */
+public class SendNotificationCommand implements Command {
+
+    private final Notifier notifier;
+    private final String message;
+
+    public SendNotificationCommand(Notifier notifier, String message) {
+        this.notifier = notifier;
+        this.message = message;
+    }
+
+    @Override
+    public void execute() {
+        notifier.send(message);
+    }
+}
 
