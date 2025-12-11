42b29db0e2c9754a8ef3ee62690104bbaea12856
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-10 23:26:19 -0300

    Update ExpensiveOperation.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/proxy/ExpensiveOperation.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/proxy/ExpensiveOperation.java
@@ -1,1 +1,8 @@
+package com.myprojecticaro.poc_designe_patterns.structural.proxy;
 
+/**
+ * Common interface for both the real object and its proxy.
+ */
+public interface ExpensiveOperation {
+    int execute(int value);
+}
