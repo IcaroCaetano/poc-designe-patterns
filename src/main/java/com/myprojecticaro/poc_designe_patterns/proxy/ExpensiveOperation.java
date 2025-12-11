3bea6af0bc5ef6804456a511347e32ba4dc0b8e5
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-10 23:30:06 -0300

    Delete src/main/java/com/myprojecticaro/poc_designe_patterns/proxy/ExpensiveOperation.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/proxy/ExpensiveOperation.java
+++
@@ -1,8 +0,0 @@
-package com.myprojecticaro.poc_designe_patterns.structural.proxy;
-
-/**
- * Common interface for both the real object and its proxy.
- */
-public interface ExpensiveOperation {
-    int execute(int value);
-}
