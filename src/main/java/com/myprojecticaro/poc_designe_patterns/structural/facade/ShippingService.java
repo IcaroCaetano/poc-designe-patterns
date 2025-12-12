883037b63e543d5f4bc8f66105e1fb850bf4c5c9
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-11 23:03:46 -0300

    Update ShippingService.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/facade/ShippingService.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/facade/ShippingService.java
@@ -1,1 +1,8 @@
+package com.myprojecticaro.poc_designe_patterns.structural.facade;
+
+public class ShippingService {
+    public void scheduleDelivery(String item) {
+        System.out.println("Delivery scheduled for item: " + item);
+    }
+}
 
