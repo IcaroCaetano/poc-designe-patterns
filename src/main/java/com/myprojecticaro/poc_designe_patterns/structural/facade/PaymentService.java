66bf9212c63367a8cc52cc49abf61d91853bca4a
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-11 23:02:33 -0300

    Update PaymentService.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/facade/PaymentService.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/facade/PaymentService.java
@@ -1,1 +1,7 @@
+package com.myprojecticaro.poc_designe_patterns.structural.facade;
 
+public class PaymentService {
+    public void processPayment(String customer, double amount) {
+        System.out.println("Payment processed for customer " + customer + " amount: " + amount);
+    }
+}
