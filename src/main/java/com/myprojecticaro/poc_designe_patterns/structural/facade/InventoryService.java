6bc5ce5d358a4ed67e3b38143b320fad06e517b1
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-11 23:03:08 -0300

    Update InventoryService.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/facade/InventoryService.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/facade/InventoryService.java
@@ -1,1 +1,13 @@
+package com.myprojecticaro.poc_designe_patterns.structural.facade;
+
+public class InventoryService {
+    public boolean checkStock(String item) {
+        System.out.println("Checking stock for: " + item);
+        return true;
+    }
+
+    public void reserveItem(String item) {
+        System.out.println("Item reserved: " + item);
+    }
+}
 
