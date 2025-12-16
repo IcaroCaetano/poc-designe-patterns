0e1369ef1f6373af0b272866f80a5142aadeb006
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-16 10:43:52 -0300

    Update UserPrototype.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/prototype/UserPrototype.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/prototype/UserPrototype.java
@@ -1,1 +1,24 @@
+package com.myprojecticaro.poc_designe_patterns.creational.prototype;
+
+public class UserPrototype implements Prototype<UserPrototype> {
+
+    private String name;
+    private String role;
+
+    public UserPrototype(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    @Override
+    public UserPrototype clone() {
+        return new UserPrototype(this.name, this.role);
+    }
+
+    @Override
+    public String toString() {
+        return "UserPrototype{name='%s', role='%s'}"
+                .formatted(name, role);
+    }
+}
 
