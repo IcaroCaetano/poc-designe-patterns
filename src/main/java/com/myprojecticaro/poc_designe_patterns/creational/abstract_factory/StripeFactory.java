ca2fc403541d9d0a55b6db2057c8ab19c43c0f9c
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-17 10:00:49 -0300

    Update StripeFactory.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/StripeFactory.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/StripeFactory.java
@@ -1,1 +1,15 @@
+package com.myprojecticaro.poc_designe_patterns.creational.abstract_factory;
+
+public class StripeFactory implements PaymentAbstractFactory {
+
+    @Override
+    public PaymentService createPaymentService() {
+        return new StripePaymentService();
+    }
+
+    @Override
+    public NotificationService createNotificationService() {
+        return new StripeNotificationService();
+    }
+}
 
