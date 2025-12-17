0bb09496de1bdddbbf863cc4e1043cd967adec7a
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-17 09:56:58 -0300

    Update StripeNotificationService.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/StripeNotificationService.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/StripeNotificationService.java
@@ -1,1 +1,9 @@
+package com.myprojecticaro.poc_designe_patterns.creational.abstractfactory;
+
+public class StripeNotificationService implements NotificationService {
+    @Override
+    public void notifyUser(String message) {
+        System.out.println("Stripe notification: " + message);
+    }
+}
 
