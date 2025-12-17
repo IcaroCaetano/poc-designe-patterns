3709faf8374efad50f98138c4f04bef8ee94c4c8
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-17 10:01:35 -0300

    Update PaypalFactory.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaypalFactory.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaypalFactory.java
@@ -1,1 +1,15 @@
+package com.myprojecticaro.poc_designe_patterns.creational.abstract_factory;
+
+public class PaypalFactory implements PaymentAbstractFactory {
+
+    @Override
+    public PaymentService createPaymentService() {
+        return new PaypalPaymentService();
+    }
+
+    @Override
+    public NotificationService createNotificationService() {
+        return new PaypalNotificationService();
+    }
+}
 
