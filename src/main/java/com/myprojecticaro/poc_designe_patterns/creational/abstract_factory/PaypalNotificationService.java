5882c4bf6a989b5f1306d9d5bd60ffc1e101245e
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-17 09:59:07 -0300

    Update PaypalNotificationService.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaypalNotificationService.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaypalNotificationService.java
@@ -1,1 +1,9 @@
+package com.myprojecticaro.poc_designe_patterns.creational.abstract_factory;
+
+public class PaypalNotificationService implements NotificationService {
+    @Override
+    public void notifyUser(String message) {
+        System.out.println("PayPal notification: " + message);
+    }
+}
 
