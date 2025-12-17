9e9dbe9ee61762a3f7b0bb0f4bfed81b5fac86f9
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-17 09:57:50 -0300

    Update PaypalPaymentService.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaypalPaymentService.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaypalPaymentService.java
@@ -1,1 +1,8 @@
+package com.myprojecticaro.poc_designe_patterns.creational.abstract_factory;
 
+public class PaypalPaymentService implements PaymentService {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paying $" + amount + " using PayPal");
+    }
+}
