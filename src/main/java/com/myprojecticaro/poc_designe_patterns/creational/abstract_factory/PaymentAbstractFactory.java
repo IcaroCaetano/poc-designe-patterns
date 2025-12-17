3508e165cd02233fe13cb541d998c5cc04a32440
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-17 10:00:06 -0300

    Update PaymentAbstractFactory.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaymentAbstractFactory.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/creational/abstract_factory/PaymentAbstractFactory.java
@@ -1,1 +1,9 @@
+package com.myprojecticaro.poc_designe_patterns.creational.abstract_factory;
+
+public interface PaymentAbstractFactory {
+
+    PaymentService createPaymentService();
+
+    NotificationService createNotificationService();
+}
 
