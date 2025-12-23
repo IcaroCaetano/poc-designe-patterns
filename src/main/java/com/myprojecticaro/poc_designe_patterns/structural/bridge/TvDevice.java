a8d1d036bc0ce459c0e8a5e406441f05e8eb6a91
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-22 22:32:40 -0300

    Update TvDevice.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/TvDevice.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/TvDevice.java
@@ -1,1 +1,20 @@
+package com.myprojecticaro.poc_designe_patterns.structural.bridge;
+
+public class TvDevice implements Device {
+
+    @Override
+    public void turnOn() {
+        System.out.println("TV turned ON");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV turned OFF");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        System.out.println("TV volume set to " + volume);
+    }
+}
 
