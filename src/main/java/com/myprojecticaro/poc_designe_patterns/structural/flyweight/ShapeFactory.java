ae07bead80f9cfa1f5eef2fd800fcf21b9f36868
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-23 18:33:17 -0300

    Update ShapeFactory.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/flyweight/ShapeFactory.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/flyweight/ShapeFactory.java
@@ -1,1 +1,14 @@
+package com.myprojecticaro.poc_designe_patterns.structural.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShapeFactory {
+
+    private static final Map<String, Shape> circleCache = new HashMap<>();
+
+    public static Shape getCircle(String color) {
+        return circleCache.computeIfAbsent(color, Circle::new);
+    }
+}
 
