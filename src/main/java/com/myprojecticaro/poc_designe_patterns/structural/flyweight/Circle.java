e57ddc238472624c8821ecf2e6ab8d7059c7c6a0
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-23 18:32:31 -0300

    Update Circle.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/flyweight/Circle.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/flyweight/Circle.java
@@ -1,1 +1,18 @@
+package com.myprojecticaro.poc_designe_patterns.structural.flyweight;
+
+public class Circle implements Shape {
+
+    private final String color;
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void draw(int x, int y) {
+        System.out.println(
+            "Drawing Circle [color=" + color + ", x=" + x + ", y=" + y + "]"
+        );
+    }
+}
 
