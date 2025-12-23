3b4063671396bd05ca7bfd3b03c5cea2df061470
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-22 22:34:55 -0300

    Update RemoteControl.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/RemoteControl.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/RemoteControl.java
@@ -1,1 +1,14 @@
+package com.myprojecticaro.poc_designe_patterns.structural.bridge;
+
+public abstract class RemoteControl {
+
+    protected Device device;
+
+    protected RemoteControl(Device device) {
+        this.device = device;
+    }
+
+    public abstract void powerOn();
+    public abstract void powerOff();
+}
 
