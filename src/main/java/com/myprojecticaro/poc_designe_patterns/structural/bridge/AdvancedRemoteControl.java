26bdc791bbcfe12412454dd131c8ca84382fc961
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-22 22:36:22 -0300

    Update AdvancedRemoteControl.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/AdvancedRemoteControl.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/AdvancedRemoteControl.java
@@ -1,1 +1,23 @@
+package com.myprojecticaro.poc_designe_patterns.structural.bridge;
+
+public class AdvancedRemoteControl extends RemoteControl {
+
+    public AdvancedRemoteControl(Device device) {
+        super(device);
+    }
+
+    @Override
+    public void powerOn() {
+        device.turnOn();
+    }
+
+    @Override
+    public void powerOff() {
+        device.turnOff();
+    }
+
+    public void mute() {
+        device.setVolume(0);
+    }
+}
 
