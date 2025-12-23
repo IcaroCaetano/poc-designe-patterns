f206760c7f67759dddfe40b2d5589ce9910a93ce
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-22 22:33:44 -0300

    Update RadioDevice.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/RadioDevice.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/structural/bridge/RadioDevice.java
@@ -1,1 +1,20 @@
+package com.myprojecticaro.poc_designe_patterns.structural.bridge;
+
+public class RadioDevice implements Device {
+
+    @Override
+    public void turnOn() {
+        System.out.println("Radio turned ON");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Radio turned OFF");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        System.out.println("Radio volume set to " + volume);
+    }
+}
 
