59807b108a587cdd072b9076d1fe7e39fa156a52
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-24 17:13:05 -0300

    Update PermissionExpression.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/PermissionExpression.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/PermissionExpression.java
@@ -1,1 +1,16 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.interpreter;
+
+public class PermissionExpression implements Expression {
+
+    private final String permission;
+
+    public PermissionExpression(String permission) {
+        this.permission = permission;
+    }
+
+    @Override
+    public boolean interpret(Context context) {
+        return context.hasPermission(permission);
+    }
+}
 
