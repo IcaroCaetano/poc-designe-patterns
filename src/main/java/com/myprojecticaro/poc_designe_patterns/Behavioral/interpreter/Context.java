c937861374de40cfc48b098c89fc804aa1b110c8
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-24 17:12:05 -0300

    Update Context.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/Context.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/Context.java
@@ -1,1 +1,17 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.interpreter;
+
+import java.util.Set;
+
+public class Context {
+
+    private final Set<String> permissions;
+
+    public Context(Set<String> permissions) {
+        this.permissions = permissions;
+    }
+
+    public boolean hasPermission(String permission) {
+        return permissions.contains(permission);
+    }
+}
 
