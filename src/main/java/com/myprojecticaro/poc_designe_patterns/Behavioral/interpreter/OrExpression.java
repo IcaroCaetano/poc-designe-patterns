312767fb6c9fb25a7b957c37399daa9045802afc
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-24 17:15:58 -0300

    Update OrExpression.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/OrExpression.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/OrExpression.java
@@ -1,1 +1,18 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.interpreter;
+
+public class OrExpression implements Expression {
+
+    private final Expression left;
+    private final Expression right;
+
+    public OrExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public boolean interpret(Context context) {
+        return left.interpret(context) || right.interpret(context);
+    }
+}
 
