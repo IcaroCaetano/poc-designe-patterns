0b1de6bd4616b3c2c5ed63af74882244a053ad50
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-24 17:14:37 -0300

    Update AndExpression.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/AndExpression.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/interpreter/AndExpression.java
@@ -1,1 +1,18 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.interpreter;
+
+public class AndExpression implements Expression {
+
+    private final Expression left;
+    private final Expression right;
+
+    public AndExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public boolean interpret(Context context) {
+        return left.interpret(context) && right.interpret(context);
+    }
+}
 
