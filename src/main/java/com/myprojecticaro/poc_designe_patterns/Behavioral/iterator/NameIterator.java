295ac5a9c3edfbdb18321c29bb1d9809c7716cca
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-29 10:25:49 -0300

    Update NameIterator.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/iterator/NameIterator.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/iterator/NameIterator.java
@@ -1,1 +1,24 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.iterator;
+
+import java.util.List;
+
+public class NameIterator implements Iterator<String> {
+
+    private final List<String> names;
+    private int index = 0;
+
+    public NameIterator(List<String> names) {
+        this.names = names;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < names.size();
+    }
+
+    @Override
+    public String next() {
+        return names.get(index++);
+    }
+}
 
