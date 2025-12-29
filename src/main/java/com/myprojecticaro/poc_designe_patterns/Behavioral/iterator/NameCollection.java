4913275e11e8813120bcab2c80fc94b69582c2ac
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-12-29 10:24:27 -0300

    Update NameCollection.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/iterator/NameCollection.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/iterator/NameCollection.java
@@ -1,1 +1,19 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.iterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NameCollection implements Aggregate<String> {
+
+    private final List<String> names = new ArrayList<>();
+
+    public void add(String name) {
+        names.add(name);
+    }
+
+    @Override
+    public Iterator<String> createIterator() {
+        return new NameIterator(names);
+    }
+}
 
