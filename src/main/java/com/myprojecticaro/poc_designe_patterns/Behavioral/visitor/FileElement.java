8f3765a7d1fd59fcbbc6ff6f81d5ecaa0bd8815b
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-05 23:20:41 -0300

    Implement FileElement class for visitor pattern

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/FileElement.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/FileElement.java
@@ -1,1 +1,26 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.visitor;
+
+public class FileElement implements Element {
+
+    private final String name;
+    private final int size;
+
+    public FileElement(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
 
