959dda031aab730aa2179f570f5b249d6b4cc3c2
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-05 23:23:37 -0300

    Update SizeCalculatorVisitor.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/SizeCalculatorVisitor.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/SizeCalculatorVisitor.java
@@ -1,1 +1,21 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.visitor;
+
+public class SizeCalculatorVisitor implements Visitor {
+
+    private int totalSize = 0;
+
+    @Override
+    public void visit(FileElement file) {
+        totalSize += file.getSize();
+    }
+
+    @Override
+    public void visit(FolderElement folder) {
+        // Folder itself has no size
+    }
+
+    public int getTotalSize() {
+        return totalSize;
+    }
+}
 
