1a3ade420e8bf1743068bda4c306854ad0c2d83c
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-05 23:24:26 -0300

    Implement PrintStructureVisitor for visitor pattern

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/PrintStructureVisitor.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/PrintStructureVisitor.java
@@ -1,1 +1,15 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.visitor;
+
+public class PrintStructureVisitor implements Visitor {
+
+    @Override
+    public void visit(FileElement file) {
+        System.out.println("File: " + file.getName());
+    }
+
+    @Override
+    public void visit(FolderElement folder) {
+        System.out.println("Folder: " + folder.getName());
+    }
+}
 
