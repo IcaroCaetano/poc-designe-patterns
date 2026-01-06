78b75c4664119cff91ed20931b99ac681ad123ff
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-05 23:22:02 -0300

    Implement FolderElement class with visitor pattern

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/FolderElement.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/visitor/FolderElement.java
@@ -1,1 +1,35 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FolderElement implements Element {
+
+    private final String name;
+    private final List<Element> children = new ArrayList<>();
+
+    public FolderElement(String name) {
+        this.name = name;
+    }
+
+    public void add(Element element) {
+        children.add(element);
+    }
+
+    public List<Element> getChildren() {
+        return children;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+        for (Element element : children) {
+            element.accept(visitor);
+        }
+    }
+}
 
