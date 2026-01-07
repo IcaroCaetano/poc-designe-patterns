01d5443ab679d73387dee8375daecf1478636a11
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-06 22:40:31 -0300

    Implement TextEditor with Memento pattern

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/memento/TextEditor.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/memento/TextEditor.java
@@ -1,1 +1,23 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.memento;
+
+public class TextEditor {
+
+    private String text;
+
+    public void write(String newText) {
+        this.text = newText;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public TextMemento save() {
+        return new TextMemento(text);
+    }
+
+    public void restore(TextMemento memento) {
+        this.text = memento.getState();
+    }
+}
 
