010a01285151be538e39b4f4d7d07c0739eb67ee
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-06 22:38:44 -0300

    Update TextMemento.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/memento/TextMemento.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/memento/TextMemento.java
@@ -1,1 +1,15 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.memento;
+
+public class TextMemento {
+
+    private final String state;
+
+    public TextMemento(String state) {
+        this.state = state;
+    }
+
+    protected String getState() {
+        return state;
+    }
+}
 
