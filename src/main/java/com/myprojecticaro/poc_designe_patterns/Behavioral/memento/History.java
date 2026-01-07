3461a5728f87dbafbccd19a8e0c89b15b2070306
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-06 22:41:27 -0300

    Implement History class for Memento pattern

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/memento/History.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/memento/History.java
@@ -4,6 +4,18 @@ import java.util.Stack;
 
 public class History {
 
-  
+    private final Stack<TextMemento> history = new Stack<>();
+
+    public void save(TextMemento memento) {
+        history.push(memento);
+    }
+
+    public TextMemento undo() {
+        return history.pop();
+    }
+
+    public boolean isEmpty() {
+        return history.isEmpty();
+    }
 }
 
