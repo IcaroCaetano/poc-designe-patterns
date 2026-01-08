36abd1bdef5ad59426db2767e9ba4df944263fc6
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-07 23:26:41 -0300

    Add constructor to User class

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/User.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/User.java
@@ -4,7 +4,14 @@ public abstract class User {
 
     protected ChatMediator mediator;
     protected String name;
+    
+    protected User(ChatMediator mediator, String name) {
+        this.mediator = mediator;
+        this.name = name;
+    }
 
+    public abstract void send(String message);
+    public abstract void receive(String message);
 
 }
 
