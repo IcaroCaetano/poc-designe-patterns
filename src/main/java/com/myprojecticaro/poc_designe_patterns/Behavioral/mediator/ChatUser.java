0db6dfef0685bde329c8227c2882cef4e78df3b7
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-07 23:27:39 -0300

    Update ChatUser.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/ChatUser.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/ChatUser.java
@@ -6,5 +6,15 @@ public class ChatUser extends User {
         super(mediator, name);
     }
 
+    @Override
+    public void send(String message) {
+        System.out.println(name + " sends: " + message);
+        mediator.sendMessage(message, this);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println(name + " receives: " + message);
+    }
 }
 
