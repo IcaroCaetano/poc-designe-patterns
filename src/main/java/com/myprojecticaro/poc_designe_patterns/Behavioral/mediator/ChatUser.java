0160bb543f63742db2ad3e85bae3ad75e6bb7a2a
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-07 23:27:25 -0300

    Add ChatUser class extending User

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/ChatUser.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/ChatUser.java
@@ -1,1 +1,10 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.mediator;
+
+public class ChatUser extends User {
+
+    public ChatUser(ChatMediator mediator, String name) {
+        super(mediator, name);
+    }
+
+}
 
