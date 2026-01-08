ac2ffbf985eaf792132813932f17e2577228d5ee
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-07 23:24:36 -0300

    Update ChatMediator.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/ChatMediator.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/mediator/ChatMediator.java
@@ -1,1 +1,7 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.mediator;
+
+public interface ChatMediator {
+    void sendMessage(String message, User user);
+    void addUser(User user);
+}
 
