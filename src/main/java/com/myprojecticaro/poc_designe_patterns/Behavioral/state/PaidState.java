ab7a27f8b927aa3a6749524130b48b51dbb83444
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-12 23:14:16 -0300

    Update PaidState.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/state/PaidState.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/state/PaidState.java
@@ -1,7 +1,10 @@
 package com.myprojecticaro.poc_designe_patterns.behavioral.state;
 
 public class PaidState implements OrderState {
-
+    @Override
+    public void next(OrderContext context) {
+        context.setState(new ShippedState());
+    }
 
 }
 
