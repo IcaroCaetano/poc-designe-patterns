1981e4f1e3bf44b1d1d93b3e9b39dbc811dc88f5
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-12 23:15:57 -0300

    Update ShippedState.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/state/ShippedState.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/state/ShippedState.java
@@ -2,6 +2,19 @@ package com.myprojecticaro.poc_designe_patterns.behavioral.state;
 
 public class ShippedState implements OrderState {
 
+    @Override
+    public void next(OrderContext context) {
+        System.out.println("Order already shipped");
+    }
 
+    @Override
+    public void previous(OrderContext context) {
+        context.setState(new PaidState());
+    }
+
+    @Override
+    public void printStatus() {
+        System.out.println("Order shipped");
+    }
 }
 
