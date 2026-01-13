3251a40ce32c317e8771e85eadd47fb410c526c2
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-12 23:17:14 -0300

    Update OrderContext.java

--- src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/state/OrderContext.java
+++ src/main/java/com/myprojecticaro/poc_designe_patterns/Behavioral/state/OrderContext.java
@@ -1,1 +1,27 @@
+package com.myprojecticaro.poc_designe_patterns.behavioral.state;
+
+public class OrderContext {
+
+    private OrderState state;
+
+    public OrderContext() {
+        this.state = new CreatedState();
+    }
+
+    protected void setState(OrderState state) {
+        this.state = state;
+    }
+
+    public void next() {
+        state.next(this);
+    }
+
+    public void previous() {
+        state.previous(this);
+    }
+
+    public void printStatus() {
+        state.printStatus();
+    }
+}
 
